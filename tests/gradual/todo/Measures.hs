06439828aa3967bb7de55821d4a9339f7979d5c4
nikivazou <nikivazou@gmail.com>
2017-03-17 15:29:44 -0400

    started reasoning about measures

--- tests/gradual/todo/Measures.hs
+++ tests/gradual/todo/Measures.hs
@@ -2,11 +2,19 @@
 
 module Measures where
 {-@ LIQUID "--gradual" @-}
-{-@ LIQUID "--extensionality" @-}
-{-@ LIQUID "--eliminate=none" @-}
-{-@ LIQUID "--scrape-used-imports" @-}
 
 -- This does not work because I need the special locality treatment for measures
 {-@ f :: x:{v:[a] | ?? } -> {v:Int | ?? } -> a -> Bool @-}
 f :: Eq a => [a] -> Int -> a -> Bool  
 f xs i y= xs!!i == y 
+
+{-@ dotProd :: x:[Int] -> y:{[Int]| ??} -> Int @-}
+dotProd :: [Int] -> [Int] -> Int
+dotProd xs ys = go (length xs)
+  where
+  	{-@ go :: Nat -> Int @-}
+  	go i | i < 0 = 0 
+  	go i = xs!!i * ys!!i + go (i-1)
+
+{-@ assume (!!) :: xs:[a] -> {v:Int | 0 <= v && v < len xs } -> a  @-}
+
